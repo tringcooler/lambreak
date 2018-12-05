e043f8fd85a8c12d24d8a6cd3fccf7c2a8d644f2
tringcooler <tringcooler@users.noreply.github.com>
2018-12-06 02:50:22 +0800

    Update frame.py

--- src/frame.py
+++ src/frame.py
@@ -1,11 +1,26 @@
 #! python3
 # coding: utf-8
 
+class _eval_stack(object):
+
+    def __init__(self):
+        pass
+
+    def push(self, dst):
+        pass
+
+    def pop(self):
+        pass
+
+    def walk(self):
+        pass
+
 class frame(object):
 
     def __init__(self, pred = None):
         self._pred = pred
         self._succ = None
+        self._stack = _eval_stack()
 
     def _append_new(self):
         assert self._succ == None 
@@ -21,3 +36,17 @@ class frame(object):
     @property
     def pred(self):
         return self._pred
+
+    @property
+    def last(self):
+        cur = self
+        while cur._succ:
+            cur = cur._succ
+        return cur
+
+    @property
+    def first(self):
+        cur = self
+        while cur._pred:
+            cur = cur._pred
+        return cur
