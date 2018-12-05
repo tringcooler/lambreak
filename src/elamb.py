beadfc123ec74e3f1b5434ce8ea6f96e6962afa4
tringcooler <tringcooler@users.noreply.github.com>
2018-12-06 00:55:04 +0800

    Create elamb.py

---
+++ src/elamb.py
@@ -0,0 +1,11 @@
+#! python3
+# coding: utf-8
+
+class elamb(object):
+
+    def __init__(self):
+        #self._pred = None
+        #self._succ = None
+
+    def append(self, dst_elamb):
+        pass
