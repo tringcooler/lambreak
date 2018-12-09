4dbd7ec0003ee4a64d18b4e43153a6a9f955b203
tringcooler <tringcooler@users.noreply.github.com>
2018-12-10 03:13:04 +0800

    metaclass for elemb

--- src/elamb.py
+++ src/elamb.py
@@ -1,7 +1,16 @@
 #! python3
 # coding: utf-8
 
-class elamb(object):
+class _elamb:
 
-    def __init__(self):
-        pass
+    def __init__(self, in_stack, out_stack):
+        self._stat = 'init'
+
+    
+
+class meta_elamb(type):
+
+    def __new__(meta_cls, name, base_cls, attrs):
+        base_cls = base_cls + (_elamb,)
+        return super(meta_elamb, meta_cls).__new__(
+            meta_cls, name, base_cls, attrs)
