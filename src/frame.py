f5f96b5a8aaa57969c5b7532d54f13e5ce9cc790
tringcooler <tringcooler@users.noreply.github.com>
2018-12-10 01:56:46 +0800

    Update frame.py

--- src/frame.py
+++ src/frame.py
@@ -1,19 +1,21 @@
 #! python3
 # coding: utf-8
 
+from functools import wraps
+
 class _eval_stack(object):
 
     def __init__(self):
-        pass
+        self._seq = []
 
     def push(self, dst):
-        pass
+        self._seq.append(dst)
 
     def pop(self):
-        pass
+        return self._seq.pop()
 
-    def walk(self):
-        pass
+    def walk_gen(self):
+        return iter(self._seq)
 
 class frame(object):
 
@@ -28,8 +30,16 @@ class frame(object):
         self._succ = new_frame
 
     @property
+    def is_last(self):
+        return not self._succ
+
+    @property
+    def is_first(self):
+        return not self._pred
+
+    @property
     def succ(self):
-        if not self._succ:
+        if self.is_last:
             self._append_new()
         return self._succ
 
@@ -40,13 +50,25 @@ class frame(object):
     @property
     def last(self):
         cur = self
-        while cur._succ:
+        while not cur.is_last:
             cur = cur._succ
         return cur
 
     @property
     def first(self):
         cur = self
-        while cur._pred:
+        while not cur.is_first:
             cur = cur._pred
         return cur
+
+    def dec_active(meth):
+        @wraps(meth)
+        def _wrapper(self, *args, **kargs):
+            if not self.is_last:
+                raise RuntimeError('frame is not active')
+            return meth(self, *args, **kargs)
+        return _wrapper
+
+    @dec_active
+    def eval(self):
+        pass
