cfcd8600e242f807eeb1106c708cf774edcdbaa0
tringcooler <tringcooler@users.noreply.github.com>
2018-12-10 03:15:38 +0800

    Revert "metaclass for elemb"
    This reverts commit 4dbd7ec0003ee4a64d18b4e43153a6a9f955b203.
    
    并不需要在elamb中记录运行信息
    因此只需要让算子成为elamb的实例
    而不需要算子成为elamb的实例类 再在stack中放入算子的实例

--- src/elamb.py
+++ src/elamb.py
@@ -1,16 +1,7 @@
 #! python3
 # coding: utf-8
 
-class _elamb:
+class elamb(object):
 
-    def __init__(self, in_stack, out_stack):
-        self._stat = 'init'
-
-    
-
-class meta_elamb(type):
-
-    def __new__(meta_cls, name, base_cls, attrs):
-        base_cls = base_cls + (_elamb,)
-        return super(meta_elamb, meta_cls).__new__(
-            meta_cls, name, base_cls, attrs)
+    def __init__(self):
+        pass
