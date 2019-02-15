ea7ad2481f0dc42ec8ad2824d55cff0a355fa336
BSoD123456 <bsod123456@gmail.com>
2019-02-15 08:49:36 +0000

    iospy commit

--- src/opelem.py
+++ src/opelem.py
@@ -37,16 +37,75 @@ class comb_rcptr(comb_sym):
 
     def eval(self, out, pool, srci, t = 0):
         super(comb_rcptr, self).eval(out, pool, srci, t)
+        print('comb')
+        self.eval_comb(out, pool)
+    
+    def eval_comb(self, out, pool):
         if pool.get('comb_ref', False):
             pool.cur['comb_ref'] = False
             pool.cur['comb_cnt'] = pool.get('comb_cnt', 0) - 1
+            return True
+        else:
+            return False
+
+class comb_rcptr_tbp(comb_rcptr):
+
+    def __init__(self):
+        super(comb_sym, self).__init__('comb_rcptr_tbp')
+
+    def eval(self, out, pool, srci, t = 0):
+        super(comb_sym, self).eval(out, pool, srci, t)
+        if self.eval_comb(out, pool):
             out.pop()
             out.pop()
             out.append(self)
+            raise opex_scan_bypass()
+            
+
+class expr_sym(opelem):
+    pass
+
+class expr_bypass(expr_sym):
+    
+    def __init__(self):
+        super(expr_bypass, self).__init__('expr_bypass')
+    
+    def eval(self, out, pool, srci, t = 0):
+        super(expr_bypass, self).eval(out, pool, srci, t)
+        if t == 0:
+            self.eval_bypass(out, pool)
+            if self.eval_comb(out, pool):
+                out.pop()
+                out.pop()
+                out.append(self)
+            else:
+                self.bypass(out, pool, srci.peek)
         else:
-            if not isinstance(srci.peek, comb_sym):
-                out.append(srci.peek)
-                raise opex_scan_bypass()
+            if pool.get('expr_cnt', 0) > 0:
+                self.bypass(out, pool, srci.peek)
+    
+    def eval_bypass(self, out, pool):
+        pool.cur['expr_cnt'] = pool.get('expr_cnt', 0) + 1
+        
+    def bypass(self, out, pool, nxop):
+        if nxop:
+            out.append(nxop)
+        if isinstance(nxop, expr_sym):
+            nxop.eval_bypass(out, pool)
+            print('expr', pool.get('expr_cnt', 0))
+        raise opex_scan_bypass()
+        
+class expr_end(expr_sym):
+    
+    def __init__(self):
+        super(expr_end, self).__init__('expr_end')
+    
+    def eval(self, out, pool, srci, t = 0):
+        super(expr_end, self).eval(out, pool, srci, t)
+        self.eval_bypass(out, pool)
+    
+    def eval_bypass(self, out, pool):
+        pool.cur['expr_cnt'] = pool.get('expr_cnt', 0) - 1
 
 class rtab_sym(opelem):
     pass
@@ -76,5 +135,19 @@ if __name__ == '__main__':
         src_s = _eval_scanner(src_q, _base_pool())
         src_s.scan()
         print(src_s.out._seq)
-    test()
+    def test2():
+        src_q = _eval_seq()
+        src_q.append(comb())
+        src_q.append(comb())
+        src_q.append(expr_bypass())
+        src_q.append(opelem('a1'))
+        src_q.append(expr_end())
+        src_q.append(opelem('a2'))
+        src_q.append(comb())
+        src_q.append(comb_rcptr())
+        src_q.append(opelem('a3'))
+        src_s = _eval_scanner(src_q, _base_pool())
+        src_s.scan()
+        print(src_s.out._seq)
+    #test2()
 
